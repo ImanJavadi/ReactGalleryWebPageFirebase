b6c00868ed81d229298390e85a30ef43b98d1d95
imanjavadi <imanjavadi69@gmail.com>
2025-04-27 10:07:46 +0330

    Creating modal and imagegrid component and styling

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,21 +1,23 @@
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
 import { useEffect, useState } from "react"
-import { db } from "../firebase/config";
+import { db } from "../firebase/config"
 
 export const useFirestore = (collectionName) => {
 
-    const [docs, setDocs] = useState();
-    useEffect(() => {
-        const q = query(collection(db, collectionName), orderBy('createddate', 'desc'));
-        const unsub = onSnapshot(q, (snapshot) => {
-            let documents = [];
-            snapshot.forEach(doc => {
-                documents.push({ id: doc.id, ...doc.data() });
-            });
-            setDocs(documents);
-        })
-        return () => unsub()
-    }, [collectionName]);
-    return { docs };
+    const [docs, setDocs] = useState([])
 
+    useEffect(() => {
+        const q = query(collection(db, collectionName), orderBy('createdAt', 'desc'))
+        const unsub = onSnapshot(q, (snapshot) => {
+            let documents = []
+            snapshot.forEach(doc => {
+                documents.push({...doc.data(), id: doc.id})
+            });
+            setDocs(documents)
+        })
+
+        return () => unsub()
+    }, [collectionName])
+
+    return { docs }
 }
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,29 +1,39 @@
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import {db, storage} from "../firebase/config";
-import { useEffect, useState } from "react";
-import { addDoc,collection,serverTimestamp } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage"
+import { useEffect, useState } from "react"
+import { db, storage } from "../firebase/config"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 
 
-export const useStorage=(file)=>{
-    const [progress,setprogress]=useState();
-    const [error,seterror]=useState();
-    const [url,seturl]=useState();
-    useEffect(()=>{
-    const collectionref=collection(db,"images");
-    const storageref=ref(storage,file.name);
-    const uploadtask=uploadBytesResumable(storageref,file); 
-    const unsub= uploadtask.on("state_changed",(snapshot)=>{
-        setprogress((snapshot.bytesTransferred/snapshot.totalBytes)*100);
-    },
-    (err)=>{seterror(err)},
-    async ()=>{
-        const geturl=await getDownloadURL(storageref);
-        seturl(geturl);
-        await addDoc(collectionref,{imageurl:url,createddate:serverTimestamp()});
-    }
-    )
-    return ()=>unsub()
-    },[file]) 
-    return {progress,error,url} 
-    
+export const useStorage = (file) => {
+    const [progress, setProgress] = useState(0)
+    const [error, setError] = useState(null)
+    const [url, setUrl] = useState(null)
+
+    useEffect(() => {
+
+        // references
+        const storageRef = ref(storage, file.name)
+        const uploadTask = uploadBytesResumable(storageRef, file)
+        
+        const collectionRef = collection(db, 'images')
+
+        const unsub = uploadTask.on('state_changed', (snapshot) => {
+            const precentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            setProgress(precentage)
+        }, (err) => {
+            setError(err)
+        }, async () => {
+            const downloadURL = await getDownloadURL(storageRef)
+            setUrl(downloadURL)
+
+            const createdAt = serverTimestamp()
+
+            await addDoc(collectionRef, {url: downloadURL, createdAt})
+        })
+
+        return () => unsub()
+        
+    }, [file])
+
+    return {progress, url, error}
 }
